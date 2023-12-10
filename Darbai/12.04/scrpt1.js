3d38e0c093e70636793eeb3dbded3486dfb20ed8
Ausra <you@example.com>
2023-12-10 08:53:34 +0200

    new

--- Darbai/12.04/scrpt1.js
+++ Darbai/12.04/scrpt1.js
@@ -6,42 +6,37 @@ a.Iškritus herbui;
 b.Tris kartus iškritus herbui;
 c.Tris kartus iš eilės iškritus herbui;
 */
-let kartai = [];
-for (let i = 0; i < 10; i++){
-    let moneta = rand(0, 1);
-    kartai.push(moneta);
-    if (kartai[i] === 1) document.write('S');
-    else document.write('H');
-}
-document.write('<br>Nauja sąlyga <br>');
-for (let i = 0; i < 10; i++){
-    let moneta = rand(0, 1);
-    kartai.push(moneta);
-    if (kartai[i] === 1) document.write('S');
-    else { break; }
-  }
-  document.write('<br>Nauja sąlyga <br>');
-  let nulis = "";
-for (let i = 0; i < 10; i++){
-    let moneta = rand(0, 1);
-    kartai.push(moneta);
-    if (kartai[i] === 1) {document.write('S')}
-    else if (kartai[i] === 0) {nulis++;
-    document.write('H');}
-    else if(nulis === 3) { break; }
-    console.log(nulis)
-  }
-  document.write('<br>Nauja sąlyga <br>');
-  function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
+
+console.log('<br>Nauja sąlyga <br>');
+
+  while (true){
+      let moneta = rand(0, 1);
+      if (moneta === 1) {console.log('S');
+    break; }
+      else {console.log('H');}
+    }
+
+  console.log('<br>Nauja sąlyga <br>');
+
+  let nulis = 0;
+  while (true){
+      let moneta = rand(0, 1);
+      if (moneta === 0) {nulis++;
+        console.log('H');}
+      else if(nulis === 3) {console.log('HHH')
+        break; }
+      else {console.log('S');}
+      
+    }
+
+  console.log('<br>Nauja sąlyga <br>');
+ 
   let metimuSkaicius = 0;
-  let metimas = rand(0, 1); //0 - herbas, 1 - skaicius
-  if (metimas === 0) console.log("H");
-  else console.log("S");
-  while (metimas === 1) {
-    // metimas = rand(0, 1);
-    if (metimas === 0) console.log("H");
-    else console.log("S");
+  
+  while (metimuSkaicius < 3) {
+  let moneta = rand(0, 1);
+  if (moneta === 0) {console.log("H");
+    metimuSkaicius++}
+  else {console.log("S");
+ metimuSkaicius = 0;}
   } 
