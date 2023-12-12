9630f2f9b7c0bda423f70e9f9dfde8d5e81b7a12
Ausra <ausra.jarmalaviciute@gmail.com>
2023-12-12 17:34:11 +0200

    all

--- Darbai/12.12/scr.js
+++ Darbai/12.12/scr.js
@@ -2,32 +2,37 @@ const people = [];
 let currentNumeration = 1;
 
 const buttonElement = document.querySelector('#add-button');
-buttonElement.addEventListener('click', ()=>{
+buttonElement.addEventListener('click', () => {
     const person = {};
     person.firstName = document.getElementById("firstNameInput").value;
-    person.lasttName = document.getElementById("firstLastInput").value;
+    person.lastName = document.getElementById("lastNameInput").value;
     person.age = document.getElementById("ageInput").value;
-    person.fnationality = document.getElementById("nationalityInput").value;
+    person.nationality = document.getElementById("nationalityInput").value;
     person.number = currentNumeration;
-
+    people.push(person);
     currentNumeration++
     console.log(person);
 
-    people.push(person);
     generateTableContent(people);
 });
 
 function generateTableContent(people)
 {
-    let ObjektaiHTML = `<tr>`
+    let ObjektaiHTML = ``;
     for(let person of people){
         ObjektaiHTML += `<tr>
+        <td>${person.number}</td>
         <td>${person.firstName}</td>
-        <td>${person.lasttName}</td>
+        <td>${person.lastName}</td>
         <td>${person.age}</td>
         <td>${person.nationality}</td>
-        </tr>`
+        </tr>`;
     }
-    const tbody = document.querySelector('table body');
+    const tbody = document.querySelector('table tbody');
     tbody.innerHTML = ObjektaiHTML;
+}
+
+function myFunction() {
+    let i = document.getElementById("cutInput").value;
+    document.getElementById("myTable").deleteRow(i);
 }
