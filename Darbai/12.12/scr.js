0198b0952ac6cea360c3d0fa26c643d31659dce1
Ausra <ausra.jarmalaviciute@gmail.com>
2023-12-13 11:19:52 +0200

    next

--- Darbai/12.12/scr.js
+++ Darbai/12.12/scr.js
@@ -1,20 +1,58 @@
 const people = [];
+const possibleNationalities = ["Lithuanian", "Latvian", "German"];
 let currentNumeration = 1;
 
 const buttonElement = document.querySelector('#add-button');
+function validateName(name) {
+   // tikrinam ar vardas atitinka reikalavimuas, nėra skaičiu ir grazinti folse- validavimas su regexu
+    let isValid = true;
+    if(!name)
+    {
+        isValid = false;
+    }
+    //regex patikrinimas, ar bent vienas toks simbolis yra
+    if(/[0-9]/.test(name) || /[!@#$%^&*()?+-.,/{}]/.test(name))
+    {
+        isValid = false;
+    }
+    return isValid;
+}
+function validateAge(age)
+{
+    let isValid = true;
+    if(!age) isValid = false;
+  
+    if(isNaN(parseInt(age))) isValid = false;
+
+    if(age < 0 || age > 200) isValid = false;
+
+    if(age % 1 !== 0)  isValid = false;
+    
+    return isValid;
+}
+function isValidNationality(nationality) {
+return possibleNationalities.include(nationality);
+}
 buttonElement.addEventListener('click', () => {
     const person = {};
     person.firstName = document.getElementById("firstNameInput").value;
     person.lastName = document.getElementById("lastNameInput").value;
     person.age = document.getElementById("ageInput").value;
     person.nationality = document.getElementById("nationalityInput").value;
-    if (person.firstName.value === "" || person.lastName === "" || person.age === "" || person.nationality === "") document.getElementById("rezultatas0").innerHTML ="Visi laukai privalo būti užpildyti";  
-    else {person.number = currentNumeration;
+    person.number = currentNumeration;
+
+    if (!validateName(person.firstName) ||
+        !validateName(person.lastName) ||
+        !validateName(person.age) ||
+        !isValidNationality(person.nationality))
+        {alert("Visi laukai privalo būti užpildyti"); 
+        return;}
+
     people.push(person);
     currentNumeration++
     console.log(person);
     
-    generateTableContent(people);}
+    generateTableContent(people);
 });
 function generateTableContent(people)
 {
@@ -34,6 +72,6 @@ function generateTableContent(people)
 function myFunction() {
     let i = document.getElementById("cutInput").value;
     if (i < people.length)document.getElementById("myTable").deleteRow(i);
-    else document.getElementById("rezultatas").innerHTML ="Tokia numeracija lentelėje neegzistuoja";  
+    else document.getElementById("rezultatas").innerHTML ="Tokia numeracija lentelėje neegzistuoja";
   }
  
