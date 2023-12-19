aba8be207512e8b4d7ac779ea4c2024a33afaa3c
Ausra <ausra.jarmalaviciute@gmail.com>
2023-12-19 19:38:59 +0200

    add

--- Darbai/12.12/scr.js
+++ Darbai/12.12/scr.js
@@ -2,29 +2,28 @@ const people = [];
 const possibleNationalities = ["Lithuanian", "Latvian", "German", "British", "Spanish", "Indian", "Japanese"];
 let currentNumeration = 1;
 
-//Elementų apsirašymas
+
+const arrayFromStorage = JSON.parse(localStorage.getItem("Objektai"));
+generateTableContent(arrayFromStorage);
+
+
 const firstNameInput = document.getElementById("firstNameInput");
 const lastNameInput = document.getElementById("lastNameInput");
 const ageInput = document.getElementById("ageInput");
 const nationalityInput = document.getElementById("nationalityInput");
 const removeElementInput = document.querySelector("#number");
 
-//Button elemento gavimas:
 const addingButtonElement = document.querySelector("#add-button");
-// const addingButtonElement = document.getElementById('add-button');
-// const addingButtonElement = document.querySelectorAll('button');
 
-//Validacijos
 function validateName(name) {
-	// boolean reiksme
+
 	let isValid = true;
 
-	//tikriname ar name reikšmė nėra undefined, null, false, arba tuščia: ""
 	if (!name) {
 		console.log("!name");
 		isValid = false;
 	}
-	// /[a-z][A-Z]/.test(name) - Regex patikrinimas, tikriname ar praeina testą
+
 	if (/[0-9]/.test(name) || /[!@#$%^&*()?/.,]/.test(name)) {
 		console.log("regex");
 		isValid = false;
@@ -47,7 +46,7 @@ function validateAge(age) {
 function isValidNationality(nationality) {
 	return possibleNationalities.includes(nationality);
 }
-//reikšmių nusinulinimas
+
 function nullifyInputValues() {
 	firstNameInput.value = "";
 	lastNameInput.value = "";
@@ -55,11 +54,13 @@ function nullifyInputValues() {
 	nationalityInput.value = "";
 }
 
-//pridejimas
+
 addingButtonElement.addEventListener("click", () => {
+	
+	
 	const person = {};
 
-	//Objekto person laukui - priskiriama inpute įvesta reikšmė
+
 	person.firstName = firstNameInput.value;
 	person.lastName = lastNameInput.value;
 	person.age = ageInput.value;
@@ -70,7 +71,7 @@ addingButtonElement.addEventListener("click", () => {
 
 	
 
-	// Įvyksta patikrinimas ar įvestos reikšmės yra tuščios
+
 	if (
 		!validateName(person.firstName) ||
 		!validateName(person.lastName) ||
@@ -81,29 +82,27 @@ addingButtonElement.addEventListener("click", () => {
 		return;
 	}
 	
-	//Naujai sukurtas objektas pridedamas prie masyvo
-	people.push(person);
-	//Numeracija padideja
-	currentNumeration++;
-	//Naujai pergeneruojama lentelė pagal visas masyvo reikšmes
-	generateTableContent(people);
-	console.log(people);
-	localStorage.setItem("Objektai", JSON.stringify(people));
 
+	people.push(person);
+	
+	currentNumeration++;
+
+	generateTableContent(people);
+	localStorage.setItem("Objektai", JSON.stringify(people));
+	
 });
 console.log(people)
 
-const arrayFromStorage = JSON.parse(localStorage.getItem("Objektai"));
-generateTableContent(arrayFromStorage);
 
-//HTML contento generavimas
+
+
 function generateTableContent(people) {
-	//Dinaminio HTML kodas
+
 	
 	let dynamicHTML = ``;
 
 	for (let person of people) {
-		//Įvyksta pridėjimas prie stringo, pridedame reikšmes dinamiškai:
+		
 		dynamicHTML += `<tr>
         <td>${person.number}</td>
         <td>${person.firstName}</td>
@@ -112,20 +111,20 @@ function generateTableContent(people) {
         <td>${person.nationality}</td>
         </tr>`;
 	}
-	//Gaunamas tbody elementas
-	const tbody = document.querySelector("table tbody");
-	//Jo reikšmė tampa dinaminio html kodas
-	tbody.innerHTML += dynamicHTML;
 	
+	const tbody = document.querySelector("table tbody");
+	
+	tbody.innerHTML = dynamicHTML;
+
 }
 
 const removingButtonElement = document.querySelector("#remove-button");
 
 removingButtonElement.addEventListener("click", () => {
-	let number = +removeElementInput.value; //is inputu visada ateina tekstas
+	let number = +removeElementInput.value;
 	removeElementInput.value = "";
 
-	//findIndex() gražina indeksą pagal elemento reikšmę. Jei toks elementas nebuvo rastas, gražina -1
+	
 	let foundIndex = people.findIndex((person) => person.number === number);
 
 	if (foundIndex === -1) {
