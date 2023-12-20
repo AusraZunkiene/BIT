be53a9350e73841fe970f3fdaa178c1b42c0df9e
Ausra <ausra.jarmalaviciute@gmail.com>
2023-12-20 12:57:00 +0200

    add

--- Paskaitos/Papildomi su mentorium/script5.js
+++ Paskaitos/Papildomi su mentorium/script5.js
@@ -12,11 +12,17 @@ let end = inputVal.substring(len-Math.floor(len/2)).toLowerCase()
 let flip = end.split("").reverse().join("");
 let flip2 = [...end].reverse().join();
 console.log(end, flip);
-if (!/^[A-Za-z]+$/.test(inputVal)){
+/*if (!/^[A-Za-z]+$/.test(inputVal)){
 alert("Įvedėte ne raides")
 }
 else if(start === flip) {
     result.innerHTML = `${inputVal.toUpperCase()} is a palindrome`
 }
 else result.innerHTML = `${inputVal.toUpperCase()} is not a palindrome`
+}*/
+
+    if(inputVal == inputVal.split('').reverse().join(''))
+    result.innerHTML = `${inputVal.toUpperCase()} is a palindrome`
+    else result.innerHTML = `${inputVal.toUpperCase()} is not a palindrome`;
+  
 }
