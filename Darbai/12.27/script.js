f4543f348e314735beb50935e67a842b7d929f4e
Ausra <ausra.jarmalaviciute@gmail.com>
2023-12-27 17:29:41 +0200

    add

--- Darbai/12.27/script.js
+++ Darbai/12.27/script.js
@@ -1,12 +1,21 @@
+const selectorius = document.querySelector('.dogs');
 fetch("https://dog.ceo/api/breeds/list/all")
 	.then((atsakymas) => atsakymas.json()) 
-    .then((atsakymas) => {console.log(atsakymas.message);
-		document.write(`"${atsakymas.message}"`);
-    });
+    .then((atsakymas) => {
+     console.log(atsakymas.message)
+     let dynamicHTML = ``;
+     for(let i=0; i < 98; i++){
+        dynamicHTML += `<select><option>${Object.keys(atsakymas.message)[i]}</option></`}
+     document.write(dynamicHTML);
+   
+
+});
+
+/*fetch("https://dog.ceo/api/breed/hound/images")
+	.then((atsakymas) => atsakymas.json()) 
+    .then((atsakymas) => {console.log(atsakymas.message[1]);
 
 
-   /* for (let i = 0; i < options.length; i++) {
-        let option = document.getElementById(".dogs");
-        option.value = options[i].value;
-        option.text = options[i].text;
-      }*/
+document.write (`<img src="${atsakymas.message[1]}"/>`)     
+});
+*/
