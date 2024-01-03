9fbabf1f7489293566798078d9e6b10fec585f40
Ausra <ausra.jarmalaviciute@gmail.com>
2024-01-03 13:56:16 +0200

    new

--- Darbai/12.28/script.js
+++ Darbai/12.28/script.js
@@ -1,77 +1,20 @@
-/*const cocktailNameFilterElement = document.querySelector('#coctail-name-filter'),
-categorySelectElement = document.querySelector('#category-select'),
-glassSelectElement = document.querySelector('#glass-type-select'),
-ingredientSelectElement = document.querySelector('#ingrediant-select'),
-luckyButtonElement = document.querySelector('#im-lucky'),
-drinksElement = document.getElementById("#drinks");
+const cocktailNameFilterElement = document.querySelector('#coctailNameFilter'),
+	categorySelectElement = document.querySelector('#categorySelect'),
+	glassSelectElement = document.querySelector('#glassTypeSelect'),
+	ingredientSelectElement = document.querySelector('#ingrediantSelect'),
+	luckyButtonElement = document.querySelector('#lucky'),
+	drinksElement = document.getElementById(".drinks");
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
-.then((response) => response.json())
-.then((response) => selectCategory(response));
+//Tris funkcijas sudedam į vieną
+function fillCategorySelect(properties, selectElement, strFieldName) {
+	let dynamicHTML = "";
+	for (const property of properties) {
+		dynamicHTML += `<option>${property[strFieldName]}</option>`;
+		}
+	selectElement.innerHTML = dynamicHTML;
+	}
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
-.then((response) => response.json())
-.then((response) => selectGlassCategory(response));
-
-fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
-.then((response) => response.json())
-.then((response) => selectIngredientCategory(response));
-
-fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-.then((response) => response.json())
-.then((response) => createRandom(response));
-
-
-function selectCategory(category) {
-    let dynamicHTML = '';
-    const categoryArr = [];
-    for(let value of category.drinks) {
-        categoryArr.push(value);
-        dynamicHTML += `<option>${(value.map(value.strCategory))}</option>`
-    }
-        categorySelectElement.innerHTML = dynamicHTML
-    
-}
-function selectGlassCategory(category) {
-    let dynamicHTML = '';
-    const categoryArr = [];
-    for(let value of category.drinks) {
-        categoryArr.push(value);
-        dynamicHTML += `<option>${JSON.stringify(value.strGlass)}</option>`
-    }
-    glassSelectElement.innerHTML = dynamicHTML
-    
-}
-
-function selectIngredientCategory(category) {
-    let dynamicHTML = '';
-    const categoryArr = [];
-    for(let value of category.drinks) {
-        categoryArr.push(value);
-        dynamicHTML += `<option>${JSON.stringify(value.strIngredient1)}</option>`
-    }
-    ingredientSelectElement.innerHTML = dynamicHTML
-    
-}
-
-
-luckyButtonElement.addEventListener('click', () => { 
-    createRandom});
-
-    function createRandom(response) {
-    let dynamicHTML = ``;
-					dynamicHTML += `<div class="drink"><img src="${response.drinks.strDrinkThumb}">
-                    <h2 class="drink-title">${response.drinks.strDrink}</h2></div>`;
-                    drinksElement.innerHTML = dynamicHTML;
-}*/
-
-const coctailNameFilterElement = document.querySelector("#coctail-name-filter"),
-	categorySelectElement = document.querySelector("#category-select"),
-	glassSelectElement = document.querySelector("#glass-type-select"),
-	ingredientSelectElement = document.querySelector("#ingredient-select"),
-	dynamicDrinksElement = document.querySelector(".drinks");
-
-const categoriesArray = [],
+/*	const categoriesArray = [],
 	drinksArray = [];
 
 async function fillSelectElements() {
@@ -103,15 +46,7 @@ async function fillSelectElements() {
 		);
 }
 
-function fillCategorySelect(properties, selectElement, strFieldName) {
-	let dynamicHTML = "";
-	for (const property of properties) {
-		dynamicHTML += `<option>${property[strFieldName]}</option>`;
-		// categoriesArray.push(property.strCategory);
-	}
-	selectElement.innerHTML = dynamicHTML;
-	// console.log(categoriesArray);
-}
+
 
 async function getAllDrinks() {
 	// https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
@@ -144,15 +79,7 @@ function generateDrinksHTML(drinks) {
 	}
 	dynamicDrinksElement.innerHTML = dynamicHTML;
 }
-//1. Selectu užpildymas duomenimis DONE
-//2. Gauname visus gerimus is API
-//3. Juos atvaizduojame
-//4. Atlikti filtracijas kokteiliams
-//5. Paieška pagal pavadinimą
-//6. Modalinio lango sukurimas
-//7. Modalinio lango uždarymas
-//8. Atsitiktinio kokteilio gavimas su mygtuku "Man sekasi"
-//www.thecocktaildb.com/api/json/v1/1/random.php
+
 
 async function initialization() {
 	// 1. selectu uzpildymas
@@ -163,7 +90,73 @@ async function initialization() {
 	console.log(drinksArray);
 	// 2. dinaminis gerimu atvaizdavimas
 }
-initialization();
+initialization();*/
+//Galima, kad nereiktų atskirai sujungti į vieną funkciją pakeičiant elementus
+
+/*fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+.then((response) => response.json())
+.then((response) => selectCategory(response));
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
+.then((response) => response.json())
+.then((response) => selectGlassCategory(response));
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+.then((response) => response.json())
+.then((response) => selectIngredientCategory(response));
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+.then((response) => response.json())
+.then((response) => createRandom(response));
+
+
+function selectCategory(category) {
+    let dynamicHTML = '';
+    const categoryArr = [];
+    for(let value of category.drinks) {
+        categoryArr.push(value);
+        dynamicHTML += `<option>${(value.map(value.strCategory))}</option>`
+    }
+        categorySelectElement.innerHTML = dynamicHTML 
+}
+
+function selectGlassCategory(category) {
+    let dynamicHTML = '';
+    const categoryArr = [];
+    for(let value of category.drinks) {
+        categoryArr.push(value);
+        dynamicHTML += `<option>${JSON.stringify(value.strGlass)}</option>`
+    }
+    glassSelectElement.innerHTML = dynamicHTML
+}
+
+function selectIngredientCategory(category) {
+    let dynamicHTML = '';
+    const categoryArr = [];
+    for(let value of category.drinks) {
+        categoryArr.push(value);
+        dynamicHTML += `<option>${JSON.stringify(value.strIngredient1)}</option>`
+    }
+    ingredientSelectElement.innerHTML = dynamicHTML
+   //Lieka kabutės, reikia nuimti 
+}*/
+
+/*
+
+luckyButtonElement.addEventListener('click', () => { 
+    createRandom});
+
+    function createRandom(response) {
+    let dynamicHTML = ``;
+					dynamicHTML += `<div class="drink"><img src="${response.drinks.strDrinkThumb}">
+                    <h2 class="drink-title">${response.drinks.strDrink}</h2></div>`;
+                    drinksElement.innerHTML = dynamicHTML;
+}
+*/
+
+
+
+
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".drinks");
 const closeButton = document.querySelector("dialog button");
