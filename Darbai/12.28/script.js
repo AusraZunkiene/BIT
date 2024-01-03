fe71b96cd52ba51d1e7f74139b77fc7bce7c4101
Ausra <ausra.jarmalaviciute@gmail.com>
2024-01-03 16:32:16 +0200

    nes

--- Darbai/12.28/script.js
+++ Darbai/12.28/script.js
@@ -1,67 +1,72 @@
 const cocktailNameFilterElement = document.querySelector('#coctailNameFilter'),
 	categorySelectElement = document.querySelector('#categorySelect'),
 	glassSelectElement = document.querySelector('#glassTypeSelect'),
-	ingredientSelectElement = document.querySelector('#ingrediantSelect'),
+	ingredientSelectElement = document.querySelector('#ingredientSelect'),
 	luckyButtonElement = document.querySelector('#lucky'),
-	drinksElement = document.getElementById(".drinks");
+	drinksElement = document.querySelector(".drinks");
 
-//Tris funkcijas sudedam į vieną
-function fillCategorySelect(properties, selectElement, strFieldName) {
+//Tris funkcijas sudedam į vieną, užpildom selectus.
+function fillSelects(fields, selectElement, strFieldName) {
 	let dynamicHTML = "";
-	for (const property of properties) {
-		dynamicHTML += `<option>${property[strFieldName]}</option>`;
+	for (const value of fields) {
+		dynamicHTML += `<option>${value[strFieldName]}</option>`;
 		}
 	selectElement.innerHTML = dynamicHTML;
-	}
+}
+//Automatinis gavimas.
+const selectValues = [];
 
-/*	const categoriesArray = [],
-	drinksArray = [];
+async function fillSelectsElements() {
+	const allUrls = [
+	"https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list",
+	"https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list",
+	"https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list",
+	];
 
-async function fillSelectElements() {
+	const allPromises = allUrls.map((url) => fetch(url).then((response) => response.json()));
+	const allValues = await Promise.all(allPromises);
+	const [allCategories, allGlasses, allIngredients] = allValues;
+
+	selectValues.categories = allCategories.drinks.map((categoryObj)=> categoryObj.strCategory);
+	selectValues.glasses = allGlasses.drinks.map((glass)=> glass.strGlass);
+	selectValues.ingredients = allIngredients.drinks.map((ingredient)=> ingredient.strIngredient1);
+
+	fillSelects(allCategories.drinks, categorySelectElement, "strCategory");
+	fillSelects(allGlasses.drinks, glassSelectElement, "strGlass");
+	fillSelects(allIngredients.drinks, ingredientSelectElement, "strIngredient1");
+
+}
+/*async function fillSelectsElements() {
 	await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
-		.then((response) => response.json())
-		.then((response) => {
-			fillCategorySelect(response.drinks, categorySelectElement, "strCategory");
-			//Map metodas grazina is objektu masyvo objektu laukus strCategory
-			categoriesArray.push(
-				...response.drinks.map((value) => value.strCategory)
-			);
-		})
-		.catch((error) => console.log(error));
+	.then((response) => response.json())
+	.then((response) => {fillSelects(response.drinks, categorySelectElement, "strCategory");
+		categoryArray.push(...response.drinks.map((value) => value.strCategory));})
 
 	await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
-		.then((response) => response.json())
-		.then((response) =>
-			fillCategorySelect(response.drinks, glassSelectElement, "strGlass")
-		);
+	.then((response) => response.json())
+	.then((response) => fillSelects(response.drinks, glassSelectElement, "strGlass"));
 
 	await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
-		.then((response) => response.json())
-		.then((response) =>
-			fillCategorySelect(
-				response.drinks,
-				ingredientSelectElement,
-				"strIngredient1"
-			)
-		);
+	.then((response) => response.json())
+	.then((response) => fillSelects(response.drinks, ingredientSelectElement, "strIngredient1"));
 }
+*/
 
+async function initialization() {
+	await fillSelectsElements();
+	await getAllDrinks();
+	generateDrinksHTML(drinksArray); 
+}
+initialization();
 
-
+const drinksArray = [];
+//pasidaryt kaip per paskaita
 async function getAllDrinks() {
-	// https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
-	for (const category of categoriesArray) {
-		let dynamicUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category.replaceAll(
-			" ",
-			"_"
-		)}`;
-		const response = await fetch(dynamicUrl);
-		const answerFromServer = await response.json();
-		for (const drink of answerFromServer.drinks) {
-			drinksArray.push(drink);
-		}
-		// drinksArray.push(answerFromServer);
-		// console.log(answerFromServer);
+	for(const category of selectValues.categories) {
+		let dynamicUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category.replaceAll(" ","_")}`;
+	const response = await fetch(dynamicUrl);
+	const answerFromServer = await response.json();
+	for (const drink of answerFromServer.drinks) drinksArray.push(drink);
 	}
 }
 
@@ -70,27 +75,62 @@ function generateDrinksHTML(drinks) {
 
 	for (let drink of drinks) {
 		dynamicHTML += `<div class="drink">
-		<img
-			src="${drink.strDrinkThumb}"
-			alt=""
-		/>
+		<img src="${drink.strDrinkThumb}" alt=""/>
 		<h2 class="drink-title">${drink.strDrink}</h2>
-	</div>`;
+		</div>`;
 	}
-	dynamicDrinksElement.innerHTML = dynamicHTML;
+	drinksElement.innerHTML = dynamicHTML;
 }
 
 
-async function initialization() {
-	// 1. selectu uzpildymas
-	await fillSelectElements();
-	console.log(categoriesArray);
-	await getAllDrinks();
-	generateDrinksHTML(drinksArray); 
-	console.log(drinksArray);
-	// 2. dinaminis gerimu atvaizdavimas
-}
-initialization();*/
+
+
+/*
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+.then((response) => response.json())
+.then((response) => createRandom(response));
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".drinks");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", () => {
+  dialog.showModal(
+
+
+    ``
+  );
+});
+
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+luckyButtonElement.addEventListener('click', () => { 
+	createRandom(response){
+			dialog.showModal(
+	
+	
+				``
+			  );
+			
+			
+			closeButton.addEventListener("click", () => {
+			  dialog.close();
+			});
+						drinksElement.innerHTML = dynamicHTML;
+	}})
+	
+
+
+*/
+
+
+
+
+
+
+
 //Galima, kad nereiktų atskirai sujungti į vieną funkciją pakeičiant elementus
 
 /*fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
@@ -104,10 +144,6 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
 fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
 .then((response) => response.json())
 .then((response) => selectIngredientCategory(response));
-
-fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-.then((response) => response.json())
-.then((response) => createRandom(response));
 
 
 function selectCategory(category) {
@@ -141,38 +177,5 @@ function selectIngredientCategory(category) {
    //Lieka kabutės, reikia nuimti 
 }*/
 
-/*
 
-luckyButtonElement.addEventListener('click', () => { 
-    createRandom});
-
-    function createRandom(response) {
-    let dynamicHTML = ``;
-					dynamicHTML += `<div class="drink"><img src="${response.drinks.strDrinkThumb}">
-                    <h2 class="drink-title">${response.drinks.strDrink}</h2></div>`;
-                    drinksElement.innerHTML = dynamicHTML;
-}
-*/
-
-
-
-
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector(".drinks");
-const closeButton = document.querySelector("dialog button");
-
-showButton.addEventListener("click", () => {
-  dialog.showModal(
-
-
-    ``
-  );
-});
-
-
-closeButton.addEventListener("click", () => {
-  dialog.close();
-});
-
-
-
+ 
