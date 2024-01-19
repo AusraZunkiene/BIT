0375711d75c083eb9b398cd88942640007d42b74
AusraZunkiene <ausra.jarmalaviciute@gmail.com>
2024-01-19 11:01:28 +0200

    all

--- Darbai/Section/app.js
+++ Darbai/Section/app.js
@@ -1,8 +1,16 @@
 const fileSystem = require("fs");
 const data = require("./data.json");
 const express = require("express");
+const session = require("express-session");
 
 const app = express();
+
+app.use(session({
+    secret: "banana",
+    saveUninitialized: true,
+    resave: false,
+    cookie: {httpOnly: true},
+}));
 
  async function readFile(){
 // Sinchroninis perskaitymo būdas- vienu metu aptarnauja vieną VENGTI
@@ -25,8 +33,27 @@ app.get("/", async(req, res)=>{
     console.log("patinka");
     data.countOfVisitors++;
     await writeFile(data);
+    req.session.userId = 1;
     res.status(200).json({message: `You are a ${data.countOfVisitors}- th visitor`,})
 })
+
+app.get('/whats-my-id', (req, res)=>{
+    if(req.session.userId){
+        console.log(req.session.userId);
+        return res.status(200).json({userId: req.session.userId});
+    }
+    else{ req.session.userId = 1;
+        return res.status(200).json({userId: req.session.userId});
+
+    }
+   return res.send("Very good");
+});
+
+app.get('/users', (req, res)=>{
+    console.log(req.session.userId);
+    res.status(200).json({message: "labas"})
+})
+
 app.listen(3000, ()=> {
     console.log('Serveris pasileido')
 })
