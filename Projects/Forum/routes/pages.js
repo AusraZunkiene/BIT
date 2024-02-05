306a4789d1f4ad63046101074af3e7e7eb8b03d7
AusraZunkiene <ausra.jarmalaviciute@gmail.com>
2024-02-05 17:41:24 +0200

    forum

--- Projects/Forum/routes/pages.js
+++ Projects/Forum/routes/pages.js
@@ -1,11 +1,14 @@
 const express = require("express");
-
 const router = express.Router();
 const UserModel = require("../models/user");
+const PostModel = require("../models/post");
+const postModifications = require("../utils/postModifications");
 
 router.get("/", async (req, res) => {
 	//index.ejs failo atvaizdavimas iš views aplanko
 
+	const posts = await postModifications.getPostsWithAuthors();
+	console.log(posts);
 	const config = {
 		title: "Fortra - best forum in the world!",
 		username: "Justelio19",
@@ -13,7 +16,8 @@ router.get("/", async (req, res) => {
 		activeTab: "Home",
 		loggedIn: !!req.session.user?.loggedIn,
 		message: req.query.message,
-		posts: await PostModel.find({}),
+		error: req.query.error,
+		posts,
 	};
 	res.render("news", config);
 	//Kartu paduodami ir parametrai EJS failui
@@ -43,7 +47,7 @@ router.get("/login", (req, res) => {
 		error: req.query.error,
 	};
 	res.render("login", config);
-	//Login routas skirtas prisijungimu
+	//Login routas skirtas prisijungimui
 });
 router.get("/my-profile", async (req, res) => {
 	// Patikrinimas ar vartotojas yra prisijungęs
@@ -68,17 +72,6 @@ router.get("/my-profile", async (req, res) => {
 	};
 	res.render("profile", config);
 });
-
-
-router.get("/forums", (req, res) => {
-	const config = {
-		activeTab: "Home",
-		loggedIn: !!req.session.user?.loggedIn,
-	};
-	res.render("forums", config);
-
-});
-
 router.get("/new-post", (req, res) => {
 	if (!req.session.user?.loggedIn) {
 		return res.redirect("/login?error=Jums reikia prisijungti prie paskyros");
@@ -89,7 +82,27 @@ router.get("/new-post", (req, res) => {
 		loggedIn: !!req.session.user?.loggedIn,
 	};
 	res.render("new-post", config);
-
+	//Kartu paduodami ir parametrai EJS failui
 });
 
+router.get("/profile/:id", async (req, res) => {
+	const user = await UserModel.find({ _id: req.params.id });
+});
+router.get("/post/:id", async (req, res) => {
+	try {
+		const post = await PostModel.find({ _id: req.params.id });
+		const user = await UserModel.findOne({ _id: "65bb46ed3052e606312d8bc4" });
+		console.log(post);
+		const config = {
+			title: "Fortra - best forum in the world!",
+			activeTab: "",
+			loggedIn: !!req.session.user?.loggedIn,
+			post,
+			user,
+		};
+		res.render("post", config);
+	} catch (err) {
+		res.redirect("/?error=Nerastas irasas");
+	}
+});
 module.exports = router;
