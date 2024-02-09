a7003b86d6020eff6fffd4fa161f995c9dc5c5b3
AusraZunkiene <ausra.jarmalaviciute@gmail.com>
2024-02-09 09:23:45 +0200

    forum

--- Projects/Forum/public/index.js
+++ Projects/Forum/public/index.js
@@ -3,9 +3,9 @@ const header = document.querySelector(".header");
 window.addEventListener("scroll", function () {
 	console.log("a");
 	if (window.scrollY > 100) {
-		header.style.backgroundColor = "#004442e2";
+		header.style.backgroundColor = "##50685e2";
 	} else {
-		header.style.backgroundColor = "#004442";
+		header.style.backgroundColor = "#50685e";
 	}
 });
 function destroyEvent(event) {
